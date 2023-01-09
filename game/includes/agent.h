c52f39a9d83e5e5be279cf160bb71a6da20c9b5c
sumukshashidhar <sumukuuu@gmail.com>
2023-01-09 18:30:28 -0500

    feat: update agent headers

--- game/includes/agent.h
+++ game/includes/agent.h
@@ -5,4 +5,17 @@
 #ifndef GAME_AGENT_H
 #define GAME_AGENT_H
 
+#include "board.h"
+
+class Agent {
+public:
+    Agent();
+    void play_game(bool verbose = false);
+private:
+    Board internal_board;
+    short generateRandomMove(short player);
+
+};
+
+
 #endif //GAME_AGENT_H
