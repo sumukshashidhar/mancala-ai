56532c073daf5a9de834b5d04e268ebfb33d2bf7
sumukshashidhar <sumukuuu@gmail.com>
2023-01-09 16:20:47 -0500

    feat: modify base game

--- game/includes/board.h
+++ game/includes/board.h
@@ -5,4 +5,27 @@
 #ifndef GAME_BOARD_H
 #define GAME_BOARD_H
 
+#include <iostream>
+
+const short BOARD_ROWS = 2;
+const short BOARD_WIDTH = 7;
+const short BOARD_LEN = BOARD_WIDTH * BOARD_ROWS;
+const short NUM_SEEDS = 5;
+
+class Board {
+public:
+    Board();
+    void printBoard();
+    void play(short player, short pos);
+private:
+    // store as a 1d array, for easier iterations and faster computation
+    short board[BOARD_LEN]{};
+    short scores[BOARD_ROWS]{};
+    short distribute(short seeds, short pos);
+    short get_next_valid_pos(short pos);
+    void karu_update();
+    void check_playability(short player);
+    short collect(short pos);
+};
+
 #endif //GAME_BOARD_H
