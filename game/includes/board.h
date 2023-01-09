543076530e69c0c578f72dfa7955990e3c3ccb60
sumukshashidhar <sumukuuu@gmail.com>
2023-01-09 16:20:10 -0500

    feat: add base game

---
+++ game/includes/board.h
@@ -0,0 +1,8 @@
+//
+// Created by Sumuk Shashidhar on 1/9/23.
+//
+
+#ifndef GAME_BOARD_H
+#define GAME_BOARD_H
+
+#endif //GAME_BOARD_H
---
+++ game/src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Sumuk Shashidhar on 1/9/23.
+//
+
+#ifndef GAME_MAIN_H
+#define GAME_MAIN_H
+
+#include "board.h"
+
+int main() {
+    Board board;
+    board.print_board();
+}
+
+
+#endif //GAME_MAIN_H
