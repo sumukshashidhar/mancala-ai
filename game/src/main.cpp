39c08f4aa94c4c794301a8d386d51b6351965fad
sumukshashidhar <sumukuuu@gmail.com>
2023-01-09 17:15:26 -0500

    feat: finish version without repop

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,15 +9,22 @@ void play_infinite() {
     Board b;
     int rounds = 0;
     bool test;
-    // Providing a seed value
-    srand((unsigned) time(nullptr));
+    std::vector<short> options;
+    short choice;
     while (true) {
-        test = b.play(0, rand() % 7);
+        // Providing a seed value
+        srand((unsigned) time(nullptr));
+        options = b.get_moves(0);
+        choice = options[rand() % options.size()];
+        test = b.play(0, choice);
         if (!test) {
             break;
         }
         rounds++;
-        test = b.play(1, rand() % 7);
+        srand((unsigned) time(nullptr));
+        options = b.get_moves(1);
+        choice = options[rand() % options.size()];
+        test = b.play(1, choice);
         if (!test) {
             break;
         }
