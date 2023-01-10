5815b7dafd9ef69ac136421203fda0282abf8fcd
sumukshashidhar <sumukuuu@gmail.com>
2023-01-09 21:33:40 -0500

    feat: add ai

--- game/includes/ai.h
+++ game/includes/ai.h
@@ -8,15 +8,19 @@
 #include "board.h"
 #include <vector>
 
-const int DEPTH = 30; // look 30 moves deep
+const int DEPTH = 20; // look 30 moves deep
 
 class AI{
 public:
     AI() = default;
     short getBestMove(Board board, short player);
+    void playTheAi(Board& board);
+    static short getUserPlayerChoice();
 private:
     short minimax(Board board, short depth, short alpha, short beta, short player);
     short evaluate(Board board);
+
+    void playUserMove(Board &board, short player);
 };
 
 #endif //GAME_AI_H
